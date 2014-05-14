9bcc61a234910cd72eb636b07f3eca79642e10d6
shinokada <okada.shin@gmail.com>
2014-05-15 08:05:53 +0900

    Add comment

--- name_index.rb
+++ name_index.rb
@@ -7,25 +7,31 @@ class NameIndex
     k =[]
     kanatop = []
     names.sort!
+    # find where name belong to in KATAKANA [0, 1, 1, 5, 5, 9]
     names.each do |s|
       KATAKANA.each_with_index do |kana, index|
         result << index if kana.include?(s[0])
       end
     end
-  
+ 
+    # zip result and name and for each array insert to array  [["イトウ"], ["カネダ", "キシモト"], [], [], [], ["ハマダ", "ババ"], [], [], [], ["ワダ"]]
     result.zip(names).each do |val|
       array[val[0]] << val[1]
     end
+
+    # reject empty array
     array.reject!{ |c| c.empty? }
   
+    # find KATAKANA top letters
     KATAKANA.each do |obj|
       kanatop << obj[0]
     end
 
+    # for each result [0,1,1,5,5,9], insert kanatop to k array
     result.each do |e|
       k << kanatop[e]
     end
-    k.uniq.zip(array)
-  
+    # create k unique and zip with array
+    k.uniq.zip(array) 
   end
 end
