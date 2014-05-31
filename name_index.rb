84d61b501fc0015e4dfbdafb5719ea008acddd0a
shinokada <okada.shin@gmail.com>
2014-05-31 15:48:43 +0900

    Refactoring

--- name_index.rb
+++ name_index.rb
@@ -1,37 +1,24 @@
-class NameIndex
+module NameIndex
   KATAKANA=['アイウエオ','カキクケコガギグゲゴ','サシスセソザジズゼゾ','タチツテトダヂヅデド','ナニヌネノ','ハヒフヘホバビブベボパピプペポ','マミムメモ','ヤユヨ','ラリルレロ','ワ']
 
-  def self.create_index(names)
-    array = Array.new(10){Array.new()}
-    result = []
-    k =[]
-    kanatop = []
-    names.sort!
-    # find where name belong to in KATAKANA [0, 1, 1, 5, 5, 9]
-    names.each do |s|
-      KATAKANA.each_with_index do |kana, index|
-        result << index if kana.include?(s[0])
+  extend self
+
+  def create_index(names)
+    sorted_names = names.sort
+    group_index = sorted_names.map do |name|
+      KATAKANA.index do |kana|
+        kana.include?(name.chr)
       end
     end
- 
-    # zip result and name and for each array insert to array  [["イトウ"], ["カネダ", "キシモト"], [], [], [], ["ハマダ", "ババ"], [], [], [], ["ワダ"]]
-    result.zip(names).each do |val|
-      array[val[0]] << val[1]
+    array = Array.new(KATAKANA.size){Array.new()}
+    group_index.zip(sorted_names) do |i, name|
+      array[i] << name
     end
-
-    # reject empty array
-    array.reject!{ |c| c.empty? }
-  
-    # find KATAKANA top letters
-    KATAKANA.each do |obj|
-      kanatop << obj[0]
+    array.reject!(&:empty?)
+    kanatop = KATAKANA.map(&:chr)
+    final = group_index.map do |i|
+      kanatop[i]
     end
-
-    # for each result [0,1,1,5,5,9], insert kanatop to k array
-    result.each do |e|
-      k << kanatop[e]
-    end
-    # create k unique and zip with array
-    k.uniq.zip(array) 
+    final.uniq.zip(array) 
   end
 end
